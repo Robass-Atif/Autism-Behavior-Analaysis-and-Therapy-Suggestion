b859b393c39f6ecc457b5a282207f8c1d253c6a3
Robass Atif <mohammadrobass@gmail.com>
2026-02-03 23:08:08 +0500

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,7 +10,12 @@ import { UploadModule } from './modules/upload/upload.module';
 import { InvitationModule } from './modules/invitation/invitation.module';
 import { PredictModule } from './modules/predict/predict.module';
 import { HealthController } from './health.controller';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { PatientsModule } from './modules/patients/patients.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { ClinicalModule } from './modules/clinical/clinical.module';
 
+import { TherapyGoalsModule } from './modules/therapy-goals/therapy-goals.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
@@ -44,6 +49,11 @@ import { ScheduleModule } from '@nestjs/schedule';
     EmailModule,
     UploadModule,
     InvitationModule,
+    PatientsModule,
+    AdminModule,
+    ClinicalModule,
+    DashboardModule,
+    TherapyGoalsModule,
     PredictModule,
   ],
   controllers: [HealthController],
